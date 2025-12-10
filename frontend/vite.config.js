400a6f2460a53b611b9e750f7ffba9fa3ed0c160
kundan-thakur61 <152484837+kundan-thakur61@users.noreply.github.com>
2025-12-10 17:33:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    commonjsOptions: {
+      include: [/socket\.io-client/, /node_modules/]
+    }
+  },
   optimizeDeps: {
     include: ['socket.io-client']
   }
